f569cdc32eff1a9c2c3313f311d96daabe9d8e42
blha303 <stevensmith.ome@gmail.com>
2013-11-14 22:51:05 +0800

    fixed json_decode

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_GET['hash'])) {
-$response = json_decode(file_get_contents("https://mediacru.sh/api/".$_GET['hash']."/exists"));
+$response = json_decode(file_get_contents("https://mediacru.sh/api/".$_GET['hash']."/exists"), true);
 if (!$response['exists']) {
     echo "File has been deleted. <a href='index.php'>Click here</a> to upload a new file!";
 } else { ?>
