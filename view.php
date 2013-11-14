32813d713a53fc6c649f66dd731f5d057cf45c5d
blha303 <stevensmith.ome@gmail.com>
2013-11-14 22:46:13 +0800

    Added 'file is already deleted' message

--- view.php
+++ view.php
@@ -1,8 +1,12 @@
 <?php
-if (isset($_GET['hash'])) { ?>
+if (isset($_GET['hash'])) {
+$response = json_decode(file_get_contents("https://mediacru.sh/api/".$_GET['hash']."/exists"));
+if (!$response['exists']) {
+    echo "File has been deleted. <a href='index.php'>Click here</a> to upload a new file!";
+} else { ?>
 <script type="text/javascript" src="https://mediacru.sh/static/mediacrush.js"></script>
 <a href="delete.php?hash=<?php echo $_GET['hash']; ?>">Click here to delete this file.</a>
 <div class="mediacrush" data-media="<?php echo $_GET['hash']; ?>"></div>
-<?php } else {
+<?php } } else {
     header('Location: /snapcrush/index.php');
 }
