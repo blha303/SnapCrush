f2eee2a7165e256e880a3185b6c378ea2817a719
blha303 <stevensmith.ome@gmail.com>
2013-11-15 18:31:07 +0800

    Made image the delete link

--- view.php
+++ view.php
@@ -5,8 +5,8 @@ if (!$response['exists']) {
     echo "File has been deleted. <a href='index.php'>Click here</a> to upload a new file!";
 } else { ?>
 <script type="text/javascript" src="https://mediacru.sh/static/mediacrush.js"></script>
-<a href="delete.php?hash=<?php echo $_GET['hash']; ?>">Click here to delete this file.</a>
-<div class="mediacrush" data-media="<?php echo $_GET['hash']; ?>"></div>
-<?php } } else {
+<a href="delete.php?hash=<?php echo $_GET['hash']; ?>"><div class="mediacrush" data-media="<?php echo $_GET['hash']; ?>"></div></a>
+<?php }
+} else {
     header('Location: /snapcrush/index.php');
 }
